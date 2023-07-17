2f5a296de9e5a2861d06002592a13f86a1911a60
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-17 11:54:39 +0300

    Created a new instance of the Tree class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+      Tree myFavoriteOakTree = new Tree(25,
+              5,TreeType.OAK);
+
+      System.out.println(myFavoriteOakTree.treeType);
 
     }
 }
